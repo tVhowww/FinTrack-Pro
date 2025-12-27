0fd0844cbc81d3be09e999b18a1086e181933cb8
tVhowww <nguyentrunghau7746@gmail.com>
2025-12-27 23:53:18 +0700

    Initial commit: Setup Docker Infra & User Entity

---
+++ backend/identity-service/src/main/java/com/fintrack/identity_service/entity/User.java
@@ -0,0 +1,36 @@
+package com.fintrack.identity_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    private LocalDate dob;
+
+    // Sau này sẽ thêm Role (Admin/User) ở đây
+}
