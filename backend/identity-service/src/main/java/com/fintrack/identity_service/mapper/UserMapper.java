92e84649717f5725301c354b551448a1f3947d36
tVhowww <nguyentrunghau7746@gmail.com>
2025-12-29 00:11:56 +0700

    FP-9 [Identity] Integrate Mapstruct

---
+++ backend/identity-service/src/main/java/com/fintrack/identity_service/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.fintrack.identity_service.mapper;
+
+import com.fintrack.identity_service.dto.request.UserCreationRequest;
+import com.fintrack.identity_service.entity.User;
+import org.mapstruct.Mapper;
+
+// componentModel = "spring" giúp bạn có thể @Autowired mapper này vào Service
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    // 1. Chuyển từ Request DTO -> Entity (Dùng khi đăng ký)
+    User toUser(UserCreationRequest request);
+
+    // 2. Chuyển từ Entity -> Response DTO (Dùng khi trả về cho user - làm sau)
+//    UserResponse toUserResponse(User user);
+}
