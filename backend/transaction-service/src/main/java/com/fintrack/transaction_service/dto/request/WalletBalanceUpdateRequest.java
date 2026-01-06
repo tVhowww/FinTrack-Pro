0f8d60bb294612c18e2d8a88da93a92d74d4434d
tVhowww <nguyentrunghau7746@gmail.com>
2026-01-06 23:34:48 +0700

    FP-41 feat: [Transaction] Implement Wallet Feign Client

---
+++ backend/transaction-service/src/main/java/com/fintrack/transaction_service/dto/request/WalletBalanceUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.fintrack.transaction_service.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WalletBalanceUpdateRequest {
+    private BigDecimal amount;
+}
---
+++ backend/transaction-service/src/main/java/com/fintrack/transaction_service/dto/response/WalletResponse.java
@@ -0,0 +1,19 @@
+package com.fintrack.transaction_service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WalletResponse {
+    private String id;
+    private String name;
+    private BigDecimal balance;
+    private String currency;
+}
