b95022c6baa94fd3dce5748ce63deecee50d7d32
tVhowww <nguyentrunghau7746@gmail.com>
2026-01-12 14:40:00 +0700

    FP-47 feat(transaction/export): [Transaction] Export Transactions to Excel

--- backend/transaction-service/src/main/java/com/fintrack/transaction_service/controller/TransactionController.java
+++ backend/transaction-service/src/main/java/com/fintrack/transaction_service/controller/TransactionController.java
@@ -8,6 +8,11 @@ import com.fintrack.transaction_service.dto.response.TransactionResponse;
 import com.fintrack.transaction_service.enums.TransactionType;
 import com.fintrack.transaction_service.service.TransactionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
@@ -17,6 +22,22 @@ import java.time.Instant;
 @RequestMapping("transactions")
 public class TransactionController {
     private final TransactionService transactionService;
+
+    @GetMapping("/export")
+    public ResponseEntity<Resource> exportTransactions(
+            @RequestParam(required = false) String walletId,
+            @RequestParam(required = false) TransactionType type,
+            @RequestParam(required = false) Instant startDate,
+            @RequestParam(required = false) Instant endDate
+    ) {
+        String filename = "transactions_export.xlsx";
+        InputStreamResource file = new InputStreamResource(transactionService.exportToExcel(walletId, type, startDate, endDate));
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                .contentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+                .body(file);
+    }
 
     @GetMapping("/statistics/monthly")
     public ApiResponse<MonthlyStatisticsResponse> getMonthlyStatistics(
