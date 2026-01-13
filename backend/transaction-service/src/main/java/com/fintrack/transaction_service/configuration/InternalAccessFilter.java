89df5da6d1b7a522e1a40eea8dc7e0e3dc40bf32
tVhowww <nguyentrunghau7746@gmail.com>
2026-01-13 10:25:56 +0700

    chore(security): unlock access swagger using add internal secret key

--- backend/transaction-service/src/main/java/com/fintrack/transaction_service/configuration/InternalAccessFilter.java
+++ backend/transaction-service/src/main/java/com/fintrack/transaction_service/configuration/InternalAccessFilter.java
@@ -5,9 +5,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class InternalAccessFilter extends OncePerRequestFilter {
@@ -15,15 +17,36 @@ public class InternalAccessFilter extends OncePerRequestFilter {
     @Value("${app.internal-secret}")
     private String internalSecret;
 
+    // Danh sách các đường dẫn được phép truy cập tự do (Swagger)
+    private final List<String> openApiEndpoints = List.of(
+            "/**/swagger-ui/**",
+            "/**/v3/api-docs/**",
+            "/**/swagger-resources/**",
+            "/**/webjars/**"
+    );
+
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        // 1. Lấy header bí mật từ request gửi đến
+        String requestUri = request.getRequestURI();
+
+        // 1. Kiểm tra xem request có nằm trong danh sách Whitelist (Swagger) không?
+        boolean isWhitelisted = openApiEndpoints.stream()
+                .anyMatch(pattern -> pathMatcher.match(pattern, requestUri));
+
+        if (isWhitelisted) {
+            // Nếu là Swagger -> Cho qua luôn, không check Secret
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 2. Nếu không phải Swagger -> Bắt buộc check Secret Key
         String requestSecret = request.getHeader("X-Internal-Secret");
 
-        // 2. So sánh với key trong file cấu hình
         if (internalSecret == null || !internalSecret.equals(requestSecret)) {
             // Nếu sai hoặc không có -> Chặn ngay lập tức
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
